8c58e1de4d73f662aab4816238aca9ea607f359b
Eddie <eddiechen47s@gmail.com>
2021-02-18 16:27:21 +0800

    jjjj

---
+++ sourcetree123/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  sourcetree123
+//
+//  Created by ktrade on 2020/3/3.
+//  Copyright © 2020 Swift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("ok")
+        view.backgroundColor = .blue
+    }
+
+
+}
+
